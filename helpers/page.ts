738ec5f0ab2dc93eb57ce777fcbbd8f2c5175853
Guido Lange <g.lange@thalia.de>
2022-02-22 08:16:37 +0100

    Get page name by best matching route

--- helpers/page.ts
+++ helpers/page.ts
@@ -1,4 +1,5 @@
 import { Pages, pages } from '../constants/page';
+import { PageConfiguration } from '../interfaces/Page';
 
 import Dict = NodeJS.Dict;
 
@@ -12,5 +13,15 @@ export const getRoute = (page: Pages, parameters?: Dict<string | number | null |
   return pages[page].route.replace(regex, (matched) => `${parameters[matched.replace(/[{}]/gi, '')] ?? ''}`);
 };
 
-export const getPageNameByRoute = (route: string): string | undefined =>
-  Object.values(pages).find((page) => page.route === route)?.name;
+export const getPageNameByRoute = (route: string): string | undefined => getBestMatchingPage(route)?.name;
+
+const getBestMatchingPage = (route: string): undefined | PageConfiguration => {
+  let page: PageConfiguration | undefined = undefined;
+
+  Object.values(pages).forEach((currPage) => {
+    if (route.match(currPage.route.replace(/{.*}/gi, '.+'))) {
+      page = !page || page.route.length < currPage.route.length ? currPage : page;
+    }
+  });
+  return page;
+};
